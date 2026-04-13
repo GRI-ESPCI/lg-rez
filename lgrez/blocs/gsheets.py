830465039551eab3163a8bbc51d2fb5eecda6781
theo_sabouret <theosabouret02@gmail.com>
2026-04-13 21:07:50 +0200

    setup check

--- lgrez/blocs/gsheets.py
+++ lgrez/blocs/gsheets.py
@@ -222,6 +222,9 @@ def get_files_in_folder(folder_id: str) -> list[dict[str, str]]:
     """
     scope = "https://www.googleapis.com/auth/drive.readonly"
     service = build("drive", "v3", credentials=_get_creds(scope))
+    print(scope)
+    print(service)
+    print((service.files().list(corpora="user",q=f"'{folder_id}' in parents",fields="files(id, fileExtension, name)")
     data = (
         service.files()
         .list(
