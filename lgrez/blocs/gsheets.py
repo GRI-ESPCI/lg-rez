ad40d87fd2745e287d60ba306fd89519b6ed6dd8
theo_sabouret <theosabouret02@gmail.com>
2026-04-13 21:27:33 +0200

    setup check

--- lgrez/blocs/gsheets.py
+++ lgrez/blocs/gsheets.py
@@ -207,7 +207,7 @@ def get_doc_content(doc_id: str) -> list[tuple[str, dict]]:
     return content
 
 
-def get_files_in_folder(folder_id: str) -> list[dict[str, str]]:
+async def get_files_in_folder(folder_id: str) -> list[dict[str, str]]:
     """Récupère le contenu binaire d'un fichier Google Drive.
     Args:
         folder_id: ID du fichier à récupérer (doit être public ou
@@ -222,9 +222,9 @@ def get_files_in_folder(folder_id: str) -> list[dict[str, str]]:
     """
     scope = "https://www.googleapis.com/auth/drive.readonly"
     service = build("drive", "v3", credentials=_get_creds(scope))   
-    tools.log(scope)
-    tools.log(service)
-    tools.log((service.files().list(corpora="user",q=f"'{folder_id}' in parents",fields="files(id, fileExtension, name)")))
+    await tools.log(scope)
+    await tools.log(service)
+    await tools.log((service.files().list(corpora="user",q=f"'{folder_id}' in parents",fields="files(id, fileExtension, name)")))
     data = (
         service.files()
         .list(
